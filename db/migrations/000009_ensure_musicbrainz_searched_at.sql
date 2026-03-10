58bbca1a05813a3195ae1b0a019c235fc0f22f30
kang-heewon <heewon.dev@gmail.com>
2026-03-10 14:59:57 +0900

    fix: restore release musicbrainz lookup marker

---
+++ db/migrations/000009_ensure_musicbrainz_searched_at.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+
+ALTER TABLE releases ADD COLUMN IF NOT EXISTS musicbrainz_searched_at TIMESTAMPTZ DEFAULT NULL;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+ALTER TABLE releases DROP COLUMN IF EXISTS musicbrainz_searched_at;
+
+-- +goose StatementEnd
